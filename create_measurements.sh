e1065f2230fa5de3f5bc268b7c35e87770b5b08e
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-06-09 14:25:27 +0200

    Sizing the map

--- create_measurements.sh
+++ create_measurements.sh
@@ -41,6 +41,7 @@ java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC07_NoCopyForDoubleST $1 $2 $3 
 java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC08_GoIntST $1 $2 $3 --batchMode ""
 java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC09_NoMergeST $1 $2 $3 --batchMode ""
 java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC10_MutateST $1 $2 $3 --batchMode ""
+java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC11_SizedMapST $1 $2 $3 --batchMode ""
 
 
 #java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC01_BaselineMT $1 $2 $3 --batchMode ""
