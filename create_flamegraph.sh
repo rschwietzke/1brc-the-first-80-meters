b384afa48e97a268444f5167d39e794781a91c2a
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-06-10 22:07:10 +0200

    No allocation version

---
+++ create_flamegraph.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+#  Copyright 2023 The original authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+CLASSPATH=target/classes/
+
+java -agentpath:/home/rschwietzke/bin/async-profiler/async-profiler-3.0-linux-x64/lib/libasyncProfiler.so=start,event=cpu,flamegraph,file=$1-cpu.html  -cp target/classes/ org.rschwietzke.devoxxpl24.$1 $2 $3 $4
+java -agentpath:/home/rschwietzke/bin/async-profiler/async-profiler-3.0-linux-x64/lib/libasyncProfiler.so=start,event=alloc,flamegraph,file=$1-alloc.html  -cp target/classes/ org.rschwietzke.devoxxpl24.$1 $2 $3 $4
--- create_measurements.sh
+++ create_measurements.sh
@@ -44,6 +44,7 @@ java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC10_MutateST $1 $2 $3 --batchMo
 java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC11_SizedMapST $1 $2 $3 --batchMode ""
 java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC12_NewMapST $1 $2 $3 --batchMode ""
 java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC13_HardcodedSetST $1 $2 $3 --batchMode ""
+java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC14_ReadBytesST $1 $2 $3 --batchMode ""
 
 
 #java -cp $CLASSPATH org.rschwietzke.devoxxpl24.BRC01_BaselineMT $1 $2 $3 --batchMode ""
