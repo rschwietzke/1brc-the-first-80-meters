e699b6accdb5d3fcc072cc0da7595a880c116495
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-06-12 22:41:28 +0200

    Flamegraph for many things

--- create_flamegraph.sh
+++ create_flamegraph.sh
@@ -16,5 +16,7 @@
 #
 CLASSPATH=target/classes/
 
+java -agentpath:/home/rschwietzke/bin/async-profiler/async-profiler-3.0-linux-x64/lib/libasyncProfiler.so=start,event=cache-misses,flamegraph,file=$1-cache-misses.html  -cp target/classes/ org.rschwietzke.devoxxpl24.$1 $2 $3 $4
 java -agentpath:/home/rschwietzke/bin/async-profiler/async-profiler-3.0-linux-x64/lib/libasyncProfiler.so=start,event=cpu,flamegraph,file=$1-cpu.html  -cp target/classes/ org.rschwietzke.devoxxpl24.$1 $2 $3 $4
 java -agentpath:/home/rschwietzke/bin/async-profiler/async-profiler-3.0-linux-x64/lib/libasyncProfiler.so=start,event=alloc,flamegraph,file=$1-alloc.html  -cp target/classes/ org.rschwietzke.devoxxpl24.$1 $2 $3 $4
+java -agentpath:/home/rschwietzke/bin/async-profiler/async-profiler-3.0-linux-x64/lib/libasyncProfiler.so=start,event=branch-misses,flamegraph,file=$1-branch-misses.html  -cp target/classes/ org.rschwietzke.devoxxpl24.$1 $2 $3 $4
