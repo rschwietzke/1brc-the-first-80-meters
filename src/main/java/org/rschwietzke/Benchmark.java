d9c1c45f4250f0403a0e20644f78aef32c8459ca
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-09-01 21:14:24 +0200

    We can now print data to file

--- src/main/java/org/rschwietzke/Benchmark.java
+++ src/main/java/org/rschwietzke/Benchmark.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -34,15 +36,19 @@ public abstract class Benchmark
 
     public static void run(final Class<? extends Benchmark> clazz, final String[] args)
     {
-    	try
-    	{
-			run(clazz.getDeclaredConstructor(), args);
-		}
-    	catch (NoSuchMethodException | SecurityException e)
-    	{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        try
+        {
+            run(clazz.getDeclaredConstructor(), args);
+        }
+        catch (NoSuchMethodException | SecurityException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -50,10 +56,10 @@ public abstract class Benchmark
      */
     static void print(final boolean batchMode, final Supplier<String> s)
     {
-    	if (!batchMode)
-    	{
-    		System.out.print(s.get());
-    	}
+        if (!batchMode)
+        {
+            System.out.print(s.get());
+        }
     }
 
     /**
@@ -67,100 +73,114 @@ public abstract class Benchmark
 
     /**
      * Just runs our test and measures things
+     * @throws IOException
      * @throws NoSuchAlgorithmException
      */
-    public static void run(final Constructor<? extends Benchmark> ctr, final String[] args)
+    public static void run(final Constructor<? extends Benchmark> ctr, final String[] args) throws IOException
     {
-    	// did we get anything?
-    	if (args.length < 3)
-    	{
-    		printError();
+        // did we get anything?
+        if (args.length < 3)
+        {
+            printError();
             return;
-    	}
+        }
 
-    	final String fileName = args[0];
+        final String fileName = args[0];
 
-    	// do we have an additional second and third?
-    	final int warmUpRuns = Integer.valueOf(args[1]);
-    	final int measurementRuns = Integer.valueOf(args[2]);
+        // do we have an additional second and third?
+        final int warmUpRuns = Integer.valueOf(args[1]);
+        final int measurementRuns = Integer.valueOf(args[2]);
 
-    	final boolean batchMode = args.length > 3 && args[3].equalsIgnoreCase("--batchmode");
+        final boolean batchMode = args.length > 3 && args[3].equalsIgnoreCase("--batchmode");
+        final boolean printToFile = args.length > 3 && args[3].equalsIgnoreCase("-o");
+        final String outputFileName = args.length > 4 ? args[4] : null;
 
-    	final String batchComment;
-    	if (batchMode && args.length < 5)
-    	{
-   			printError();
-   			return;
-    	}
-    	else if (batchMode && args.length == 5)
-    	{
-    		batchComment = args[4].isBlank() ? "none" : args[4];
-    	}
-    	else
-    	{
-    		batchComment = "none";
-    	}
+        final String batchComment;
+        if (batchMode && args.length < 5)
+        {
+            printError();
+            return;
+        }
+        else if (batchMode && args.length == 5)
+        {
+            batchComment = args[4].isBlank() ? "none" : args[4];
+        }
+        else
+        {
+            batchComment = "none";
+        }
 
-    	Benchmark.print(batchMode, () -> "==== WARMUP ==================\n");
-   		var results = measure(ctr, Mode.WARMUP, warmUpRuns, fileName, batchMode);
+        Benchmark.print(batchMode, () -> "==== WARMUP ==================\n");
+        var results = measure(ctr, Mode.WARMUP, warmUpRuns, fileName, batchMode);
 
         Benchmark.print(batchMode, () -> "==== MEASUREMENT ==================\n");
         results = measure(ctr, Mode.MEASUREMENT, measurementRuns, fileName, batchMode);
 
         Benchmark.print(batchMode, () -> "==== RESULT ========================\n");
         long total = 0;
+        String lastResult = null;
+
         for (BenchmarkResult result : results)
         {
-        	total += result.runtime;
+            total += result.runtime;
+            lastResult = result.data;
         }
         final int mean = Math.round(total / results.size());
 
         //  verify the checksum
-        final var crcs = results.stream().map(r -> r.crc).sorted().distinct().toList();
+        final var crcs = results.stream().map(r -> r.getCRC()).sorted().distinct().toList();
         if (crcs.size() > 1)
         {
-        	throw new RuntimeException("CRC results vary!!!");
+            throw new RuntimeException("CRCs vary!!!");
         }
 
         if (batchMode)
         {
-        	var clazzName = ctr.getDeclaringClass().getSimpleName();
-        	System.out.print(String.format("%s,%s,%s,%d%n", clazzName, crcs.get(0), batchComment, mean));
+            var clazzName = ctr.getDeclaringClass().getSimpleName();
+            System.out.print(String.format("%s,%s,%s,%d%n", clazzName, crcs.get(0), batchComment, mean));
         }
         else
         {
-        	System.out.println(String.format("Mean Measurement Runtime: %d ms", mean));
+            System.out.println(String.format("Mean Measurement Runtime: %d ms", mean));
+        }
+
+        // do we want to print?
+        if (printToFile)
+        {
+            var o = Files.newBufferedWriter(Paths.get(outputFileName));
+            o.write(lastResult);
+            o.close();
         }
     }
 
     private static List<BenchmarkResult> measure(final Constructor<? extends Benchmark> ctr,
-    		Mode mode, int iterationCount, String fileName,
-    		final boolean batchMode)
+            Mode mode, int iterationCount, String fileName,
+            final boolean batchMode)
     {
         final List<BenchmarkResult> results = new ArrayList<>();
 
-    	for (int i = 0; i < iterationCount; i++)
-    	{
-    		Benchmark benchmark = null;
-			try
-			{
-				benchmark = ctr.newInstance();
-			}
-			catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-					| InvocationTargetException | SecurityException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+        for (int i = 0; i < iterationCount; i++)
+        {
+            Benchmark benchmark = null;
+            try
+            {
+                benchmark = ctr.newInstance();
+            }
+            catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                    | InvocationTargetException | SecurityException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
-			var result = measure(benchmark, fileName);
-	        results.add(result);
+            var result = measure(benchmark, fileName);
+            results.add(result);
 
-	        Benchmark.print(batchMode,
-	        		() -> String.format(mode == Mode.WARMUP ? "Warmup Runtime (%s): %d ms%n" : "Measurement Runtime (%s): %d ms%n", result.crc, result.runtime));
-    	}
+            Benchmark.print(batchMode,
+                    () -> String.format(mode == Mode.WARMUP ? "Warmup Runtime (%s): %d ms%n" : "Measurement Runtime (%s): %d ms%n", result.getCRC(), result.runtime));
+        }
 
-    	return results;
+        return results;
     }
 
     public static BenchmarkResult measure(final Benchmark benchmark, final String fileName)
@@ -171,32 +191,41 @@ public abstract class Benchmark
             final String data = benchmark.run(fileName);
             final long end = System.currentTimeMillis();
 
-            // checksum the data to ensure we always produce the same output
-            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            final byte[] encodedhash = digest.digest(data.getBytes(StandardCharsets.UTF_8));
-
-            return new BenchmarkResult(end - start, Base64.getEncoder().encodeToString(encodedhash));
+            return new BenchmarkResult(end - start, data);
         }
         catch (IOException ioe)
         {
             System.err.println("Darn... what just happened?");
             ioe.printStackTrace();
         }
-        catch (NoSuchAlgorithmException alg)
-        {
-            System.err.println("SHA-256, where are you?");
-            alg.printStackTrace();
-        }
 
         return null;
     }
 
-    private static record BenchmarkResult(long runtime, String crc)
+    private static record BenchmarkResult(long runtime, String data)
     {
+        public String getCRC()
+        {
+            try
+            {
+                // checksum the data to ensure we always produce the same output
+                final MessageDigest digest = MessageDigest.getInstance("SHA-512");
+
+                final byte[] encodedhash = digest.digest(data.getBytes(StandardCharsets.UTF_8));
+                return Base64.getEncoder().encodeToString(encodedhash);
+            }
+            catch (NoSuchAlgorithmException alg)
+            {
+                System.err.println("SHA-512, where are you?");
+                alg.printStackTrace();
+            }
+
+            return null;
+        }
     }
 
     private enum Mode
     {
-    	WARMUP, MEASUREMENT
+        WARMUP, MEASUREMENT
     }
 }
