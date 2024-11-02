c8c1f189f1992f22914120c36a26c8a25d6a0a93
Rene Schwietzke <r.schwietzke@xceptance.com>
2024-11-02 01:33:47 +0100

    Window start

--- src/main/java/org/rschwietzke/util/BasicWindowedBuffer.java
+++ src/main/java/org/rschwietzke/util/BasicWindowedBuffer.java
@@ -1,11 +1,20 @@
 package org.rschwietzke.util;
 
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 
 public class BasicWindowedBuffer
 {
-    public BasicWindowedBuffer(final ByteBuffer source)
+    private final RandomAccessFile file;
+    private final byte[] buffer;
+
+    private final long from;
+    private final long to;
+
+    public BasicWindowedBuffer(final RandomAccessFile file)
     {
-        
+        this.file = file;
+        this.from = 0;
+        this.to = source.
     }
 }
